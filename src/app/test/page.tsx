5bb1e3cf24ada7943ae83a562f24efb5ecb15f4f
Xamalf <34105901+Xamalf@users.noreply.github.com>
2024-05-27 13:22:01 +0200

    Added csv for results

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -5,54 +5,60 @@ import * as MPHands from "@mediapipe/tasks-vision";
 
 function FileLoader() {
     const grec = useRef<MPHands.GestureRecognizer | null>(null);
-    const [findings, setFindings] = useState<[string, number]>(["none", 0]);
+    const findings = useRef<string>("label;predicted;score;time\n");
+    const [done, setDone] = useState<boolean>(false);
 
     useEffect(() => {
         initializeMediaPipe().then(() => {
             processImages();
         });
     }, []); 
-
     
     async function initializeMediaPipe() {
 
         const vision = await MPHands.FilesetResolver.forVisionTasks(
             "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm");
       
-          grec.current = await MPHands.GestureRecognizer.createFromOptions(vision, {
+        grec.current = await MPHands.GestureRecognizer.createFromOptions(vision, {
             numHands: 1,
             runningMode: "IMAGE",
             customGesturesClassifierOptions: {scoreThreshold: 0.8},
             baseOptions: {
-              modelAssetPath: "./gesture_recognizer.task",
-              delegate: "GPU",
+                modelAssetPath: "./gesture_recognizer.task",
+                delegate: "GPU",
             },
-          });
+        });
     }
 
     // Processes the images
     async function processImages() {        
         const startTime = Date.now();
-        await performRecognition();
+        for (let folder of ["peace", "pointer"]) {
+            await performRecognition(folder);
+        }
         const EndTime = Date.now();
         const elapsedTime = EndTime - startTime;
+        setDone(true);
         console.log("Elapsed time: " + elapsedTime + "ms");
     }
 
-    async function performRecognition() {
-        for (let i = 0; i < 3; i++) {
+    async function performRecognition(folderName: string) {
+        for (let i = 1; i <= 3; i++) {
             try {
-                const image = await loadImage('/' + i + '.jpg');
+                const image = await loadImage("/testData/"+ folderName + "/" + i + '.jpg');
                 if (grec.current) {
                     const startTime2 = Date.now();
                     let results = await recognizeGesture(image);
                     const EndTime2 = Date.now();
-                    console.log("Results for image " + i + ".jpg");
-                    console.log(results?.gestures[0][0].categoryName);
-                    setFindings([results?.gestures[0][0].categoryName, results?.gestures[0][0].score]);
+                    const elapsedTime = EndTime2 - startTime2;
+                    findings.current += [folderName, 
+                        results?.gestures[0] ? results?.gestures[0][0]?.categoryName : "-", 
+                        results?.gestures[0] ? results?.gestures[0][0]?.score : "-", 
+                        elapsedTime].join(";") + "\n";
                 } 
             
             } catch {
+                findings.current += folderName + ";error\n";
                 console.error("Error processing image " + i + ".jpg");
             }
            
@@ -76,6 +82,7 @@ function FileLoader() {
     return (
         <main>
             <h1>Files</h1>
+            {done ? <a href={URL.createObjectURL(new Blob([findings.current], {type: 'text/csv'}))} download='TestData.csv'>Download Data</a> : <p>Running Tests</p>}
         </main>
     );
 }
